1a2f8e56df635b33ca003dc57d2ab7d07c26e12e
keithmburu <keithmburu1@gmail.com>
2019-12-07 16:04:56 -0500

    Keith 12/7 4:04pm

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ pygame.init()
 
 # Creating and naming the Pong window
 screen = pygame.display.set_mode((700, 500), pygame.FULLSCREEN)
-pygame.display.set_caption("Pong v2")
+pygame.display.set_caption("Pong V2")
 
 # Hiding pointer so it does not get in the way
 pygame.mouse.set_visible(False)
@@ -42,6 +42,8 @@ all_sprites_list.add(paddleB)
 # Using clock to set the number of times screen updates each second
 clock = pygame.time.Clock()
 
+
+
 # Setting a boolean that controls when the game is open or closed
 Continue = True
 
@@ -49,11 +51,15 @@ Continue = True
 font = pygame.font.Font("/Users/Keith/Downloads/product-sans/Product Sans Regular.ttf", 60)
 font1 = pygame.font.Font("/Users/Keith/Downloads/zorque/zorque.ttf", 120)
 font2 = pygame.font.Font("/Users/Keith/Downloads/product-sans/Product Sans Regular.ttf", 20)
+font3 = pygame.font.Font("/Users/Keith/Downloads/zorque/zorque.ttf", 40)
+
+keys = pygame.key.get_pressed()
 
 # Establishing a requirement of condition that will trigger the title screen
-Start = 1
+Start = 2
 # Establishing a requirement of condition that will trigger the main game loop
 Run = 0
+timer = 0
 # Running game until Continue is False
 while Continue:
     events = pygame.event.get()
@@ -74,25 +80,35 @@ while Continue:
                 Continue = False
             # If return key specifically is pressed
             if event.key == pygame.K_RETURN:
-                Run += 1  # Allowing main game loop to run
+                    if timer == 0:  # First return press.
+                        timer = 0.001  # Start the timer.
+                    # Press again before 0.5 seconds to double press.
+                    elif timer < 5:
+                        print('double press')
+                        timer = 0
+                        Run += 2  # Starting main game loop
+                        # Playing main background track when game is started
+                        winsound.PlaySound("/Users/Keith/Downloads/backtrack.wav", winsound.SND_ASYNC | winsound.SND_LOOP)
 
-    if Run == 1:
-        # Playing main background track when game is started
-        winsound.PlaySound("/Users/Keith/Downloads/backtrack.wav", winsound.SND_ASYNC | winsound.SND_LOOP)
-        # Clearing the screen and redrawing net and score bar
+
+
+    # Increase timer after mouse was pressed the first time.
+    if timer != 0:
+        timer += 0.1
+        # Reset after 0.5 seconds.
+        if timer >= 5:
+            Run += 1  # Allowing person vs computer choice
+            timer = 0
+
+    if Run == 2:
+        # Clearing the screen
         screen.fill(BLACK)
-        pygame.display.flip()
-        # Drawing the net
-        pygame.draw.line(screen, WHITE, [350, 0], [350, 75], 5)
         # Drawing the score bar
+        pygame.draw.line(screen, WHITE, [350, 0], [350, 75], 5)
         pygame.draw.line(screen, WHITE, [700, 75], [0, 75], 10)
+        # Drawing the net
         for dash in range(75, 500, 30):
             pygame.draw.line(screen, WHITE, [350, dash - 10], [350, dash], 1)
-
-        all_sprites_list.update()
-
-        # Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
-        all_sprites_list.draw(screen)
 
         # Displaying the initial score:
         scoreA = 0
@@ -102,7 +118,6 @@ while Continue:
         text = font.render(str(scoreB), 1, WHITE)
         screen.blit(text, (525, 1))
 
-        keys = pygame.key.get_pressed()
         # Moving the paddles when player A uses the arrow keys or player B uses the "W/S" keys
         if keys[pygame.K_w]:
             paddleA.moveUp(5)
@@ -115,24 +130,42 @@ while Continue:
 
         all_sprites_list.update()
 
-        # Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
+        # Drawing the sprites
         all_sprites_list.draw(screen)
 
         # Refreshing screen with lines and sprites
         pygame.display.flip()
 
         # Passing 30 frames in every second
-        clock.tick(120)
+        clock.tick(30)
     # If Run is still 0, meaning return has not been pressed
-    else:
+    if Run == 0:
         # If Start is still 1, meaning title screen code has yet to run
-        while Start:
+        while Start == 2:
             # Playing title background track on title screen
             winsound.PlaySound("/Users/Keith/Downloads/doot doots.wav", winsound.SND_ASYNC | winsound.SND_LOOP)
-            text = font1.render("Pong v2", 1, WHITE)
+            text = font1.render("Pong V2", 1, WHITE)
             screen.blit(text, (90, 90))
             text = font2.render("Press Enter to start", 1, WHITE)
-            screen.blit(text, (255, 415))
+            screen.blit(text, (270, 415))
+            # Refreshing screen with text
+            pygame.display.flip()
+            # Changing Start so that title screen displays only once
+            Start = 1
+    # If Run is 1, meaning that Return has been pressed a second time
+    if Run == 1:
+        # If Start is still 1, meaning title screen code has yet to run
+        while Start == 1:
+            # Playing title background track on title screen
+            winsound.PlaySound("/Users/Keith/Downloads/doot doots.wav", winsound.SND_ASYNC | winsound.SND_LOOP)
+            # Clearing the screen
+            screen.fill(BLACK)
+            text = font3.render("Do you want to play against a", 1, WHITE)
+            screen.blit(text, (20, 90))
+            text = font3.render("person or the computer?", 1, WHITE)
+            screen.blit(text, (70, 150))
+            text = font2.render("Press P or C", 1, WHITE)
+            screen.blit(text, (300, 415))
             # Refreshing screen with text
             pygame.display.flip()
             # Changing Start so that title screen displays only once
@@ -140,6 +173,10 @@ while Continue:
 
 # Stopping game engine when Continue is False:
 pygame.quit()
+
+
+
+
 
 
 
